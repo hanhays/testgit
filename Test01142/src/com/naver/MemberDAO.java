3de101ae4428f26a847e18db4a87b9a7f82d677a
hanhays <orcalovehyla@icloud.com>
2020-01-14 15:40:19 +0900

    test01142

---
+++ Test01142/src/com/naver/MemberDAO.java
@@ -0,0 +1,49 @@
+package com.naver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class MemberDAO {
+	
+	private final String DRIVER = "oracle.jdbc.OracleDriver";
+	private final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private final String USER = "ca2";
+	private final String PASSWORD = "ca2";
+	
+	public MemberDAO() {
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void insert(MemberDTO dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "insert into member2(id,name,age) values(?,?,?)";
+		try {
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getName());
+			pstmt.setInt(3, dto.getAge());
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+}
